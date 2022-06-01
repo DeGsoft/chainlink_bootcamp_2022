8d13e09d078875d7e2865066b88a7ae156492974
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-06-01 13:38:41 -0500

    Ejercicio bonus

--- day1_e3/contracts/MyFirstContract.sol
+++ day1_e3/contracts/MyFirstContract.sol
@@ -2,15 +2,22 @@ pragma solidity ^0.8.7;
 
 contract MyFirstContract {
 
-   uint256 number;
-
+   uint256 number = 0;
 
    function changeNumber(uint256 _num) public {
-       number = _num;
+       number = number + _num;
    }
 
+   function addNumbers(uint _num1, uint _num2) public {
+       number = _num1 + _num2;
+   }   
 
    function getNumber() public view returns (uint256){
        return number;
    }
+
+   function getNumberMultiplied(uint _num) public view returns (uint256){
+       return (_num * number);
+   }   
+
 }
