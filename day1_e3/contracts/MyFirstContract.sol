3e542507a42f82c1873bfd503cb0a521c6768df9
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-06-01 13:43:44 -0500

    Revert "Ejercicio bonus"
    This reverts commit 8d13e09d078875d7e2865066b88a7ae156492974.

--- day1_e3/contracts/MyFirstContract.sol
+++ day1_e3/contracts/MyFirstContract.sol
@@ -2,22 +2,15 @@ pragma solidity ^0.8.7;
 
 contract MyFirstContract {
 
-   uint256 number = 0;
+   uint256 number;
+
 
    function changeNumber(uint256 _num) public {
-       number = number + _num;
+       number = _num;
    }
 
-   function addNumbers(uint _num1, uint _num2) public {
-       number = _num1 + _num2;
-   }   
 
    function getNumber() public view returns (uint256){
        return number;
    }
-
-   function getNumberMultiplied(uint _num) public view returns (uint256){
-       return (_num * number);
-   }   
-
 }
