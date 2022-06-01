b9e2ee6f513c19aca9b4e454815f0731232e81cd
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-06-01 14:35:23 -0500

    Añadir un Array

--- day1_e3/contracts/MyFirstContract.sol
+++ day1_e3/contracts/MyFirstContract.sol
@@ -3,14 +3,22 @@ pragma solidity ^0.8.7;
 contract MyFirstContract {
 
    uint256 number;
-
+   string[] names;
 
    function changeNumber(uint256 _num) public {
        number = _num;
    }
 
+   function addName(string memory _name) public {
+       names.push(_name);
+   }
 
-   function getNumber() public view returns (uint256){
+   function getName(uint _index) public view returns (string memory) {
+       return names[_index];
+   }
+
+   function getNumber() public view returns (uint256) {
        return number;
    }
+
 }
