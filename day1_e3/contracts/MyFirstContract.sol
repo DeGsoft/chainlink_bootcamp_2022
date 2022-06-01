fcb79c1d0ecc685fed524bdb37236caa1b691ab8
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-06-01 10:47:23 -0500

    Ejercicio 3
    Mi primer contrato inteligente
    MyFirstContract.sol

---
+++ day1_e3/contracts/MyFirstContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.7;
+
+contract MyFirstContract {
+
+   uint256 number;
+
+
+   function changeNumber(uint256 _num) public {
+       number = _num;
+   }
+
+
+   function getNumber() public view returns (uint256){
+       return number;
+   }
+}
