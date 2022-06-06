178b283253da8256534a930b7900b2a3491762e3
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2022-06-06 16:03:31 -0500

    Traer precio LINK/USD

---
+++ day1_e4/PriceConsumer.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.7;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() public {
+        priceFeed = AggregatorV3Interface(0xd8bD0a1cB028a31AA859A21A3758685a95dE4623);
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timestamp,
+            uint80 answeredInRound            
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+}
